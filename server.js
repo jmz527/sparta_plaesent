dcadf63d1a79e50c60f162f168112fc24bfc98cf
James Rutledge <jarutledge2@gmail.com>
2017-03-19 12:39:54 -0400

    heroku dynamically assigns ports - good to know

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
