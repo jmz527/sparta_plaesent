8efeb0010d8ee812237a0477e9ff38d3709f1faf
James Rutledge <jarutledge2@gmail.com>
2017-03-20 06:04:42 -0400

    putting together a new server

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-var express = require('express')
-var path = require('path')
-var app = express()
-var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
-})
+/**
+ * Module dependencies.
+ */
 
-app.listen(port, function () {
-  console.log('Example app listening on port '+port+'!')
-})
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var errorHandler = require('errorhandler');
+var logger = require('morgan')
+
+// This should refer to the local React and gets installed via `npm install` in
+// the example.
+var reactViews = require('express-react-views');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('build', __dirname + '/build');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine());
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+app.locals.something = 'value';
+app.locals.qaz = 'qut';
+
+app.get('/', routes.index);
+// app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ views/index.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var Layout = require('./layout');
+
+
+
+class Index extends React.Component {
+  render() {
+    return (
+      <Layout title={this.props.title}>
+        <div id="root">{/* root element of react component tree */}
+
+          <h1>Hello World</h1>
+
+
+        </div>
+      </Layout>
+    );
+  }
+}
+
+Index.propTypes = {
+  title: React.PropTypes.string
+};
+
+module.exports = Index;
