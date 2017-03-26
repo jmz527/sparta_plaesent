faa53e8fca1aee4a4d79155845d4697054a076f5
James Rutledge <jarutledge2@gmail.com>
2017-03-25 23:23:52 -0400

    final touches

--- app/components/main.js
+++ app/components/main.js
@@ -13,7 +13,7 @@ const Main = () => (
 		display: "none",
 		backgroundImage: "url('/imgs/sample.png')",
 		backgroundColor: "teal",
-		opacity: "0.6",
+		opacity: "0.2",
 		position: "absolute",
 		zIndex: 5000,
 		top: "0", left: "0", right: "0", width: "100%", minHeight: "2725px"}}/>
